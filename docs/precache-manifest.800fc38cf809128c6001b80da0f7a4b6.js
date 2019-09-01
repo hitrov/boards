a33f1a7dfe7baa057be74bad1c3f0f7136609d59
Alexander Hitrov <github.com@hitrov.com>
2019-09-01 23:56:51 +0300

    build in docs dir for github pages.

---
+++ docs/precache-manifest.800fc38cf809128c6001b80da0f7a4b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8defe34517c77e9b43f4ad5a9a4d45f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53a978550cba51cb2a81",
+    "url": "/static/css/2.22d6b5ec.chunk.css"
+  },
+  {
+    "revision": "4122d5c8e23c1a4830cd",
+    "url": "/static/css/main.c814c2fe.chunk.css"
+  },
+  {
+    "revision": "53a978550cba51cb2a81",
+    "url": "/static/js/2.4b892c4b.chunk.js"
+  },
+  {
+    "revision": "4122d5c8e23c1a4830cd",
+    "url": "/static/js/main.3d74e92a.chunk.js"
+  },
+  {
+    "revision": "8bfeaba470e11ec1297b",
+    "url": "/static/js/runtime~main.deb8fdb2.js"
+  }
+]);
