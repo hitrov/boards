3658433903705c6941e144f4ea7b8eee60f071ec
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-07 07:34:04 +0000

    Deploy hitrov/boards to github.com/hitrov/boards.git:gh-pages

---
+++ precache-manifest.f2bd54b75096d1892f6241f002dfe10a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "354a2ba12f379bae3e0c3465afe16720",
+    "url": "/boards/index.html"
+  },
+  {
+    "revision": "53a978550cba51cb2a81",
+    "url": "/boards/static/css/2.22d6b5ec.chunk.css"
+  },
+  {
+    "revision": "80a1a70b6130805a60ed",
+    "url": "/boards/static/css/main.d77898ae.chunk.css"
+  },
+  {
+    "revision": "53a978550cba51cb2a81",
+    "url": "/boards/static/js/2.4b892c4b.chunk.js"
+  },
+  {
+    "revision": "80a1a70b6130805a60ed",
+    "url": "/boards/static/js/main.755b9858.chunk.js"
+  },
+  {
+    "revision": "652888e056eb740f9030",
+    "url": "/boards/static/js/runtime~main.1eee8e4e.js"
+  }
+]);
